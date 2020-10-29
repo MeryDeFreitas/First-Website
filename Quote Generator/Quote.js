5f8c0213cff125414172927de792fd6c901b8148
Mery F. Dos Santos <mery.dossantos@AES.COM>
2020-10-28 22:20:11 -0300

    Agregue icono a la página

--- Quote Generator/Quote.js
+++ Quote Generator/Quote.js
@@ -9,10 +9,6 @@ let quotes = [
     '“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.” – Inspirational Quote By Vince Lombardi',
     '“If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.” – Steve Jobs',
     '“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.” – Rob Siltanen',
-    // '“Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.” – Og Mandino',
-    // '“Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur.” – Mohnish Pabrai',
-    // '“We May Encounter Many Defeats But We Must Not Be Defeated.” – Maya Angelou',
-    // '“Knowing Is Not Enough; We Must Apply. Wishing Is Not Enough; We Must Do.” – Johann Wolfgang Von Goethe',
 ];
 
 let numero = 0;
